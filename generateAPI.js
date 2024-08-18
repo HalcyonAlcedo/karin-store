3711e470b2dd96eda85312e76b7eb5728ea211b1
HalcyonAlcedo <41666148+HalcyonAlcedo@users.noreply.github.com>
2024-08-18 15:27:50 +0800

    fix: 修复构建问题

--- generateAPI.js
+++ generateAPI.js
@@ -12,7 +12,14 @@ const loadPlugins = () => {
 
 const generateAPI = () => {
   const plugins = loadPlugins();
-  const outputPath = path.join(__dirname, 'api', 'plugins.json');
+  const apiDir = path.join(__dirname, 'api');
+
+  // 如果api目录不存在，则创建它
+  if (!fs.existsSync(apiDir)) {
+    fs.mkdirSync(apiDir);
+  }
+
+  const outputPath = path.join(apiDir, 'plugins.json');
   fs.writeFileSync(outputPath, JSON.stringify(plugins, null, 2));
 };
 
